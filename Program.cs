c02bf60a66fd6463efc0a611d22ff3557220f55f
Jakub <jakubszymanzki@gmail.com>
2026-02-09 12:04:24 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hi
+{
+    class Jayden
+    {
+        static void Main()
+        {
+            Console.WriteLine("is jayden marcus dood? Y/N");
+            string choice = Console.ReadLine().ToLower();
+
+            if (choice == "n")
+            {
+                Console.WriteLine("Vermoord Jayden marcus...");
+            }
+            else if (choice == "y")
+            {
+                Console.WriteLine("gefeliciteerd ik hou van je");
+            }
+        }
+    }
+}
